69ab358006390359fabe953549c036be0614fc07
Cayque Santos <cayque_santos16@hotmail.com>
2022-07-20 21:09:10 -0300

    Criar migrations das tabelas assets e assets_types.

---
+++ database/migrations/2022_07_20_234143_create_assets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assets', function (Blueprint $table) {
+            $table->id();
+            $table->char('codigo', 10);
+            $table->char('descricao', 100);
+            $table->foreignId('id_assets_type')->constrained('assets_types');
+            $table->boolean('e_excluido');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assets');
+    }
+};
