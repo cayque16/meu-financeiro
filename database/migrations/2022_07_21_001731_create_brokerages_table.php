d8265240b34daac6548bd3c7a23a6761ad606187
Cayque Santos <cayque_santos16@hotmail.com>
2022-07-20 21:25:44 -0300

    Criar migrations das tabelas brokerages e purchases.

---
+++ database/migrations/2022_07_21_001731_create_brokerages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brokerages', function (Blueprint $table) {
+            $table->id();
+            $table->char('nome', 150);
+            $table->char('site', 100);
+            $table->char('cnpj', 14);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brokerages');
+    }
+};
