98f656f4f6ef48375c9091968bc1cfc093528e4b
Cayque Santos <cayque_santos16@hotmail.com>
2022-07-31 10:04:25 -0300

    Resolvendo conflito.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/teste', function () {
+    return view('teste');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/', function () {
+        return view('home');
+    });
 });
