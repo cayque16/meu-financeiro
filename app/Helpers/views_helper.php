37d14eb57bf8c81502c0fb5b24277c10c2ce966d
Cayque Santos <cayque_santos16@hotmail.com>
2022-08-03 19:45:56 -0300

    Diminuir a fonte do botao voltar e colocar margem em todos os lados.

--- app/Helpers/views_helper.php
+++ app/Helpers/views_helper.php
@@ -8,7 +8,7 @@ function getBtnLink($tipo, $link = '#')
         ButtonType::EDITAR => ['class' => 'btn-outline-info', 'msg' => '', 'icon' => 'pen', 'divOpen' => "", 'divClose' => ""],
         ButtonType::EXCLUIR => ['class' => 'btn-outline-danger', 'msg' => '', 'icon' => 'trash', 'divOpen' => "", 'divClose' => ""],
         ButtonType::INCLUIR => ['class' => 'btn-success btn-add', 'msg' => 'Incluir', 'icon' => 'plus', 'divOpen' => "<div class=\"div-btn-add\">", 'divClose' => "</div>"],
-        ButtonType::VOLTAR => ['class' => 'btn-outline-success', 'msg' => 'Voltar', 'icon' => 'arrow-left', 'divOpen' => "", 'divClose' => ""],
+        ButtonType::VOLTAR => ['class' => 'btn-voltar btn-outline-success', 'msg' => 'Voltar', 'icon' => 'arrow-left', 'divOpen' => "", 'divClose' => ""],
     ];
 
     $button = "<a class=\"btn %s\" href=\"$link\"><i class=\"fas fa-%s\"></i> %s</a>";
