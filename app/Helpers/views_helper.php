e3930341bc671c41581901e100920ed17eefda27
Cayque Santos <cayque_santos16@hotmail.com>
2022-08-10 18:15:04 -0300

    Melhorar metodo getBtnLink.

--- app/Helpers/views_helper.php
+++ app/Helpers/views_helper.php
@@ -5,13 +5,14 @@ use App\Enums\ButtonType;
 function getBtnLink($tipo, $link = '#')
 {
     $arrayDados = [
-        ButtonType::EDITAR => ['class' => 'btn-outline-info', 'msg' => '', 'icon' => 'pen', 'divOpen' => "", 'divClose' => ""],
-        ButtonType::EXCLUIR => ['class' => 'btn-outline-danger', 'msg' => '', 'icon' => 'trash', 'divOpen' => "", 'divClose' => ""],
-        ButtonType::INCLUIR => ['class' => 'btn-success btn-add', 'msg' => 'Incluir', 'icon' => 'plus', 'divOpen' => "<div class=\"div-btn-add\">", 'divClose' => "</div>"],
-        ButtonType::VOLTAR => ['class' => 'btn-voltar btn-outline-success', 'msg' => 'Voltar', 'icon' => 'arrow-left', 'divOpen' => "", 'divClose' => ""],
+        ButtonType::EDITAR => ['id' => 'editar', 'title' => 'Editar', 'class' => 'btn-outline-info', 'msg' => '', 'icon' => 'pen', 'divOpen' => "", 'divClose' => ""],
+        ButtonType::DESATIVAR => ['id' => 'desativar', 'title' => 'Desativar', 'class' => 'btn-outline-danger', 'msg' => '', 'icon' => 'ban', 'divOpen' => "", 'divClose' => ""],
+        ButtonType::INCLUIR => ['id' => 'incluir', 'title' => 'Incluir novo registro', 'class' => 'btn-success btn-add', 'msg' => 'Incluir', 'icon' => 'plus', 'divOpen' => "<div class=\"div-btn-add\">", 'divClose' => "</div>"],
+        ButtonType::VOLTAR => ['id' => 'voltar', 'title' => 'Voltar para a página anterior', 'class' => 'btn-voltar btn-outline-success', 'msg' => 'Voltar', 'icon' => 'arrow-left', 'divOpen' => "", 'divClose' => ""],
+        ButtonType::ATIVAR => ['id' => 'ativar', 'title' => 'Ativar', 'class' => 'btn-outline-success', 'msg' => '', 'icon' => 'check', 'divOpen' => "", 'divClose' => ""],
     ];
 
-    $button = "<a class=\"btn %s\" href=\"$link\"><i class=\"fas fa-%s\"></i> %s</a>";
+    $button = "<a id='%s'  title='%s'  class=\"btn %s\" href=\"$link\"><i class=\"fas fa-%s\"></i> %s</a>";
 
-    return sprintf($arrayDados[$tipo]['divOpen'].$button.$arrayDados[$tipo]['divClose'], $arrayDados[$tipo]['class'], $arrayDados[$tipo]['icon'], $arrayDados[$tipo]['msg']);
+    return sprintf($arrayDados[$tipo]['divOpen'].$button.$arrayDados[$tipo]['divClose'], $arrayDados[$tipo]['id'], $arrayDados[$tipo]['title'], $arrayDados[$tipo]['class'], $arrayDados[$tipo]['icon'], $arrayDados[$tipo]['msg']);
 }
