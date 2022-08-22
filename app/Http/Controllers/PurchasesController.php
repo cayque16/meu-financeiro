d31cd80fae67f62f69cc0b29dc37ff67b736d1f5
Cayque Santos <cayque_santos16@hotmail.com>
2022-08-22 17:32:33 -0300

    Terminar index das compras.

--- app/Http/Controllers/PurchasesController.php
+++ app/Http/Controllers/PurchasesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Purchase;
+use App\Enums\ButtonType;
+use App\Enums\Status;
 
 class PurchasesController extends MyControllerAbstract
 {
@@ -13,11 +15,29 @@ class PurchasesController extends MyControllerAbstract
     
     protected function getCabecalho()
     {
-
+        return [
+            'Id',
+            'Data',
+            'Data Criação',
+            'Data Atualização',
+            ['label' => 'Ações','no-export' => true, 'width' => 5]
+        ];
     }
 
     protected function getTabela($dados)
     {
-        
+        $data = [];
+        foreach($dados as $dado) {
+            $botao = $dado->e_excluido ?  ButtonType::ATIVAR : ButtonType::DESATIVAR;
+            $eExcluido = $dado->e_excluido ? Status::ATIVADO : Status::DESATIVADO;
+            $data[] = [
+                $dado->id, 
+                formataDataBr($dado->data, false),
+                formataDataBr($dado->created_at),
+                formataDataBr($dado->updated_at),
+                "<nobr>".getBtnLink(ButtonType::EDITAR, "/purchases/edit/$dado->id")."  ".getBtnLink($botao, "/purchases/enable/$dado->id/$eExcluido")."</nobr>"
+            ];
+        }
+        return $data;
     }
 }
