6b4560317ca3ec0e0117528c65732c12bd34f31d
Cayque Santos <cayque_santos16@hotmail.com>
2022-08-23 17:03:05 -0300

    Adicionar metodo sltBrokerages.

--- app/Models/Brokerage.php
+++ app/Models/Brokerage.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\Status;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Brokerage extends MyModelAbstract
@@ -13,5 +14,16 @@ class Brokerage extends MyModelAbstract
     public function __construct()
     {
         parent::__construct($this);
+    }
+
+    public function sltBrokerages($arrayStatus = [Status::ATIVADO])
+    {
+        $result = Brokerage::select('id', 'nome')
+            ->whereIn('e_excluido', $arrayStatus)
+            ->orderBy('nome')
+            ->get()
+            ->toArray();
+
+        return array_column($result, 'nome', 'id');
     }
 }
