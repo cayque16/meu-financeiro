83376fa4495cee117f2931af6c92b3c42f32905c
Cayque Santos <cayque_santos16@hotmail.com>
2022-08-26 20:27:42 -0300

    Adicionar funcionalidades para criar tabela de ativos de forma dinamica ao se adicionar uma nova compra.

--- app/Http/Controllers/PurchasesController.php
+++ app/Http/Controllers/PurchasesController.php
@@ -3,14 +3,160 @@
 namespace App\Http\Controllers;
 
 use App\Models\Purchase;
+use App\Models\Asset;
+use App\Models\Brokerage;
 use App\Enums\ButtonType;
 use App\Enums\Status;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use App\Http\Requests\StorePurchaseRequest;
 
 class PurchasesController extends MyControllerAbstract
 {
+    private static $TAXA_TOTAL = 'taxaTotal';
+    private static $TOTAL_NOTA = 'totalNota';
+    private static $ATIVO_ID = 'ativoId';
+    private static $ATIVO_NOME = 'ativoNome';
+    private static $PRECO = 'preco';
+    private static $QUANTIDADE = 'quantidade';
+    private static $ARRAY_DADOS = 'arrayDados';
+
+    private static $KEY_RESPOSTA = 'sucesso';
+    private static $KEY_MSG = 'msg';
+
+
     public function __construct()
     {
+        $this->setTextoMsg('Compra');
         parent::__construct((new Purchase), 'purchases');
+        $this->assets = new Asset();
+        $this->brokerages = new Brokerage();
+    }
+
+    public function create()
+    {
+        $this->setDados(
+            'assets',
+            $this->assets->sltAssets()
+        );
+
+        $this->setDados(
+            'brokerages',
+            $this->brokerages->sltBrokerages()
+        );
+
+        return parent::create();
+    }
+
+    public function store(StorePurchaseRequest $request)
+    {
+        // dd($request->input('data'));
+        $this->modelBase->data = formataDataBd($request->input('data'));
+        $this->modelBase->id_brokerages = $request->input('id_brokerages');
+        $retorno = $this->modelBase->save();
+        dd($retorno);
+        /* $taxaTotal = Session::get(self::$TAXA_TOTAL);
+        $arrayDados = unserialize(Session::get(self::$ARRAY_DADOS));
+        $totalNota = Session::get(self::$TOTAL_NOTA);
+        dd($arrayDados);
+        foreach($arrayDados as $ativos) {
+            $taxaAtivo = $this->getTaxaAtivo($dado[2], $dado[3], $totalNota, $taxaTotal);
+        }*/
+        
+    }
+
+    public function adicionaAtivos(Request $request)
+    {
+        $validacao = $this->validaEntrada($request);
+        if(!$validacao[self::$KEY_RESPOSTA]) {
+            return $validacao;
+        }
+        
+        Session::put(self::$TAXA_TOTAL, $request->input(self::$TAXA_TOTAL));
+
+        if(Session::has(self::$TOTAL_NOTA)) {
+            $totalNota = Session::get(self::$TOTAL_NOTA);
+            $totalNota += $request->input(self::$PRECO) * $request->input(self::$QUANTIDADE);
+            Session::put(self::$TOTAL_NOTA, $totalNota);
+        } else {
+            Session::put(self::$TOTAL_NOTA, $request->input(self::$PRECO) * $request->input(self::$QUANTIDADE));
+        }
+
+        $dadosRequest = $this->getDadosRequest($request);
+
+        if(Session::has(self::$ARRAY_DADOS)) {
+            $arrayDados = unserialize(Session::get(self::$ARRAY_DADOS));
+            $total = $request->input(self::$PRECO) * $request->input(self::$QUANTIDADE);
+            $totalNota += $total;
+            $arrayDados[] = $dadosRequest;
+            Session::put(self::$ARRAY_DADOS, serialize($arrayDados));
+        } else {
+            $arrayDados[] = $dadosRequest;
+            Session::put(self::$ARRAY_DADOS, serialize($arrayDados));
+        }
+
+        $taxaTotal = Session::get(self::$TAXA_TOTAL);
+        $dados = unserialize(Session::get(self::$ARRAY_DADOS));
+        
+        $retorno[self::$KEY_MSG] = '';
+        $totalNota = Session::get(self::$TOTAL_NOTA);
+        foreach($dados as $dado) {
+            $taxaAtivo = $this->getTaxaAtivo($dado[2], $dado[3], $totalNota, $taxaTotal);
+            $retorno[self::$KEY_MSG] .= "
+            <tr>
+                <td>".$dado[1]."</td>
+                <td>".formata_moeda($dado[2])."</td>
+                <td>".$dado[3]."</td>
+                <td>".formata_moeda($taxaAtivo)."</td>
+                <td>".formata_moeda(($dado[2]*$dado[3])+$taxaAtivo)."</td>
+            </tr>";
+        }
+        $retorno[self::$KEY_RESPOSTA] = true;
+        return json_encode($retorno);
+    }
+
+    private function getTaxaAtivo($preco, $quantidade, $totalNota, $taxaTotal)
+    {
+        $total = $preco * $quantidade;
+        return round(($total/$totalNota)*$taxaTotal, 2);
+    }
+
+    private function validaEntrada($request)
+    {
+        $retorno[self::$KEY_RESPOSTA] = true;
+        if(is_null($request->input(self::$TAXA_TOTAL))) {
+            $validar[] = 'Total Taxas';
+        }    
+        if($request->input(self::$ATIVO_ID) == 0) {
+            $validar[] = 'Ativo';
+        }
+        if(empty($request->input(self::$PRECO))) {
+            $validar[] = 'Preço';
+        }
+        if(empty($request->input(self::$QUANTIDADE))) {
+            $validar[] = 'Quantidade';
+        } 
+        
+        if(isset($validar)) {
+            $retorno[self::$KEY_RESPOSTA] = false;
+            $retorno[self::$KEY_MSG] = '<ul>';
+            foreach($validar as $regra) {
+                $retorno[self::$KEY_MSG] .= "<li>O campo $regra é obrigatório.</li>";
+            }
+            $retorno[self::$KEY_MSG] .= '</ul>';
+        }
+
+        return $retorno;
+    }
+
+    private function getDadosRequest($request)
+    {
+        return [
+            $request->input(self::$ATIVO_ID),
+            $request->input(self::$ATIVO_NOME),
+            $request->input(self::$PRECO),
+            $request->input(self::$QUANTIDADE),
+        ];    
     }
     
     protected function getCabecalho()
