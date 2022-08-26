d08af54f0f532f9be2ffa35b0aafc67f689b3630
Cayque Santos <cayque_santos16@hotmail.com>
2022-08-26 20:26:31 -0300

    Adicionar a classe StorePurchaseRequest.

---
+++ app/Http/Requests/StorePurchaseRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePurchaseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'data' => 'required',
+            'id_brokerages' => 'required',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'id_brokerages' => 'corretora',
+        ];
+    }
+}
