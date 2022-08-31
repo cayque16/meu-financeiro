361c65374b638f14da0da93bf41f880e2da61980
Cayque Santos <cayque_santos16@hotmail.com>
2022-08-31 20:03:30 -0300

    Adicionar botao exibir.

--- app/Helpers/views_helper.php
+++ app/Helpers/views_helper.php
@@ -10,6 +10,7 @@ function getBtnLink($tipo, $link = '#')
         ButtonType::INCLUIR => ['id' => 'incluir', 'title' => 'Incluir novo registro', 'class' => 'btn-success btn-add', 'msg' => 'Incluir', 'icon' => 'plus', 'divOpen' => "<div class=\"div-btn-add\">", 'divClose' => "</div>"],
         ButtonType::VOLTAR => ['id' => 'voltar', 'title' => 'Voltar para a página anterior', 'class' => 'btn-voltar btn-outline-success', 'msg' => 'Voltar', 'icon' => 'arrow-left', 'divOpen' => "", 'divClose' => ""],
         ButtonType::ATIVAR => ['id' => 'ativar', 'title' => 'Ativar', 'class' => 'btn-outline-success', 'msg' => '', 'icon' => 'check', 'divOpen' => "", 'divClose' => ""],
+        ButtonType::EXIBIR => ['id' => 'exibir', 'title' => 'Exibir', 'class' => 'btn-outline-warning', 'msg' => '', 'icon' => 'eye', 'divOpen' => "", 'divClose' => ""],
     ];
 
     $button = "<a id='%s'  title='%s'  class=\"btn %s\" href=\"$link\"><i class=\"fas fa-%s\"></i> %s</a>";
