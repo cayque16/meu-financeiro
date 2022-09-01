b18aedea0bd72f9980d03b440e1d33a1fe9c5196
Cayque Santos <cayque_santos16@hotmail.com>
2022-09-01 20:01:12 -0300

    Adicionar metodo hashNomeDeArquivos.

--- app/Helpers/hash_helper.php
+++ app/Helpers/hash_helper.php
@@ -4,3 +4,8 @@ function getUniqId()
 {
     return md5(uniqid(rand(), true));
 }
+
+function hashNomeDeArquivos($nomeOriginal, $idReferencia, $tabelaReferencia)
+{
+    return md5($nomeOriginal.$idReferencia.$tabelaReferencia);
+}
