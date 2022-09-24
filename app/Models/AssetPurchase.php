35c97fdeaee2d5e3b59ff506e79ff8678ffbc6bc
Cayque Santos <cayque_santos16@hotmail.com>
2022-09-24 09:11:29 -0300

    Adicionar o metodo sqlCalculaTotalInvestidoPorAno.

--- app/Models/AssetPurchase.php
+++ app/Models/AssetPurchase.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Enums\TabelaReferencia;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\DB;
 
 class AssetPurchase extends MyModelAbstract
 {
@@ -30,5 +31,32 @@ class AssetPurchase extends MyModelAbstract
             ->where('purchase_id', '=', $idCompra)
             ->get()
             ->toArray();
+    }
+
+    public function sqlCalculaTotalInvestidoPorAno($ano = 0)
+    {
+        $query = AssetPurchase::select(
+                'a.codigo', 
+                DB::raw('SUM(quantidade) as qtde'), 
+                DB::raw('SUM(valor_unitario * quantidade + taxas) as total'),
+                DB::raw('MIN(valor_unitario) as valor_min'),
+                DB::raw('MAX(valor_unitario) as valor_max')
+            )
+            ->join('assets as a', 'asset_id', '=', 'a.id')
+            ->join('purchases as p', 'purchase_id', '=', 'p.id')
+            ->groupBy('a.id')
+            ->orderBy('a.codigo');
+
+        if($ano) {
+            $query->whereYear('p.data', $ano);
+        }
+        
+        $retorno = $query->get()->toArray();
+
+        array_walk($retorno, function(&$dado) {
+            $dado['media'] = $dado['total'] / $dado['qtde'];
+        });
+
+        return $retorno;
     }
 }
