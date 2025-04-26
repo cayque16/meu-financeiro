9d2c9e9be4f0d020f3a16ef6dd6b945539411f57
Cayque Santos <cayque_santos16@hotmail.com>
2025-04-26 14:42:20 -0300

    feat: adicionado objeto de valor que representa um uuid

---
+++ tests/Unit/Domain/UuidTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit\Domain;
+
+use Core\Domain\ValueObject\Uuid;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class UuidTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $uuid = new Uuid('6e864e97-9b7d-4532-a540-e2636460512f');
+        $this->assertSame('6e864e97-9b7d-4532-a540-e2636460512f', $uuid->__toString());
+    }
+
+    public function testRandom()
+    {
+        $uuid = Uuid::random();
+        $this->assertNotNull($uuid);
+    }
+
+    public function testInvalidUuid()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Uuid('invalid-uuid');
+    }
+}
