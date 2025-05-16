89428f25b3700960f117119485d7d18f23f2c206
Cayque Santos <cayque_santos16@hotmail.com>
2025-05-15 23:54:31 -0300

    feature: adicionada a tabela currencies.

---
+++ database/migrations/2025_05_15_234852_create_currencies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->char('name', 25);
+            $table->char('symbol', 5);
+            $table->char('iso_code', 3);
+            $table->integer('split');
+            $table->integer('decimals')->default(2);
+            $table->char('description', 100);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
