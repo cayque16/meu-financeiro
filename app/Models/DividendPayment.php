de12d9255f535303c7c90996f0a68d82671c9ad9
Cayque Santos <cayque_santos16@hotmail.com>
2025-05-16 07:38:38 -0300

    feature: adicionada a model DividendPayment.

---
+++ app/Models/DividendPayment.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DividendPayment extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'id',
+        'asset_id',
+        'date',
+        'type',
+        'amount',
+        'currency_id',
+        'deleted_at',
+        'created_at',
+    ];
+
+    public $incrementing = false;
+
+    protected $casts = [
+        'id' => 'string',
+        'asset_id' => 'string',
+        'currency_id' => 'string',
+        'created_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
+}
---
+++ tests/Unit/App/Models/DividendPaymentUnitTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit\App\Models;
+
+use App\Models\DividendPayment;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DividendPaymentUnitTest extends ModelTestCase
+{
+    protected function model(): Model
+    {
+        return new DividendPayment();
+    }
+
+    protected function traits(): array
+    {
+        return [
+            HasFactory::class,
+            SoftDeletes::class,
+        ];
+    }
+    
+    protected function fillable(): array
+    {
+        return [
+            'id',
+            'asset_id',
+            'date',
+            'type',
+            'amount',
+            'currency_id',
+            'deleted_at',
+            'created_at',
+        ];
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'id' => 'string',
+            'asset_id' => 'string',
+            'currency_id' => 'string',
+            'created_at' => 'datetime',
+            'deleted_at' => 'datetime',
+        ];
+    }
+}
