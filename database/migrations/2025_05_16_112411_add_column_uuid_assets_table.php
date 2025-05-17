28e3170719a31961cba0f29ef154cc785a04fd07
Cayque Santos <cayque_santos16@hotmail.com>
2025-05-17 10:30:24 -0300

    feature: adicionada a coluna uuid na tabela assets.

---
+++ database/migrations/2025_05_16_112411_add_column_uuid_assets_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Asset;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table("assets", function (Blueprint $table) {
+            $table->uuid("uuid")->nullable()->after("id");
+        });
+        Asset::whereNull('uuid')->chunk(100, function ($types) {
+            foreach ($types as $type) {
+                $type->uuid = Str::uuid();
+                $type->save();
+            }
+        });
+        Schema::table('assets', function (Blueprint $table) {
+            $table->uuid('uuid')->nullable(false)->unique()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("assets", function (Blueprint $table) {
+            $table->dropColumn("uuid");
+        });
+    }
+};
