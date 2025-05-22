671df97f952f4dc94f51113a938e972f0ca5d4c3
Cayque Santos <cayque_santos16@hotmail.com>
2025-05-21 23:30:46 -0300

    feature: add softdelete na tabela assets.

---
+++ database/migrations/2025_05_21_232918_add_softdelete_assets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Asset;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table("assets", function (Blueprint $table) {
+            $table->softDeletes();
+        });
+        Asset::where('e_excluido', '=', 1)->chunk(100, function ($types) {
+            foreach ($types as $type) {
+                $type->deleted_at = now();
+                $type->save();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("assets", function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+    }
+};
